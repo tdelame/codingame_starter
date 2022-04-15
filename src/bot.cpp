8288d946925e498a4ccf1cac85a218be980bbd65
Tom Delame <tdelame@gmail.com>
2022-04-15 19:07:37 +0200

    refactor: customize starter project to reflect my current bots

--- src/bot.cpp
+++ src/bot.cpp
@@ -1,10 +1,31 @@
-/** This file is parsed by a script to produce a single file source for CodinGame.
- * Every # include <...> are kept as is, while all # include "..." are replaced with the content
- * of the file. To simplify the merge process, put definitions in headers.*/
 # include "configuration.h"
+# include "BitStream.h"
+# include "CrashHandler.h"
+
+// Interface to specialize for the game
+struct CodinGameOutput { void finish() const noexcept { fprintf(stdout, "\n"); fflush(stdout);} };
+struct LocalOutput { void finish() const noexcept {} };
+struct Bot {
+  virtual int loop() = 0;
+  virtual void initialize(FILE* input, BitStream& bs, LocalOutput& output) = 0;
+  virtual void step(FILE* input, BitStream& bs, LocalOutput& output) = 0;
+  virtual void reload(BitStream& bs) = 0;
+};
+
+struct FirstBot: public Bot {
+  int loop() override {
+    CodinGameOutput output;
+    BitStream bs;
+    return 0;
+  }
+
+  void initialize(FILE* input, BitStream& bs, LocalOutput& output) override {}
+  void step(FILE* input, BitStream& bs, LocalOutput& output) override {}
+  void reload(BitStream& bs) override {}
+};
 
 int main() {
-  configuration::initialize();
-  // Write your main loop here
-  return 0;
+  fprintf(stderr, "[CRASH HANDLER] %s\n", CrashHandler::install() ? "initialized" : "NOT INITIALIZED");
+  fprintf(stderr, "Bot is a %s version\n", is_codingame ? "CodinGame" : "Local");
+  return FirstBot{}.loop();
 }
--- src/configuration.h
+++ src/configuration.h
@@ -1,30 +1,50 @@
-// # define CG_ARENA           // define this symbol for the release version in CodinGame arena
-# ifdef CG_ARENA
-// Try to improve the performance of the bot by helping the compiler to understand we want optimizations
-#  pragma GCC optimize("O3","unroll-loops","omit-frame-pointer","inline")
-#  pragma GCC option("arch=native","tune=native","no-zero-upper")
-#  pragma GCC target("avx")
-#  define TRAP __builtin_trap()// function to call when something unexpected happen in the arena
+# pragma once
+# ifndef __POPCNT__
+#  pragma GCC optimize("Ofast","unroll-loops","omit-frame-pointer","inline")// improve code optimization
+#  pragma GCC option("arch=native","tune=native","no-zero-upper")           // tuning compilation flags
+#  pragma GCC target("bmi,bmi2,lzcnt,popcnt")                               // bit manipulation
+#  pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2")          // SIMD
+#  pragma GCC target("movbe")                                               // byte swap
+#  pragma GCC target("aes,pclmul,rdrnd")                                    // encryption
+#  define TRAP __builtin_trap()                                             // halt the program in the arena
+#  undef _GLIBCXX_DEBUG
+static constexpr bool is_codingame = true;
 # else
-# ifdef _WIN32
-#  define _CRT_SECURE_NO_WARNINGS
-# endif
 #  include <csignal>
-#  define HAS_DEBUG_LOG        // allow logger::debug(...) commands to output text in stderr
-#  define HAS_INFO_LOG         // allow logger::info(...) commands to output text in stderr
-#  define HAS_CHECKS           // all Check(...), Expects(...), and Ensures(...) commands are kept in the produced binary
-#  define PROFILE              // allow the profiler to collect and print statistics
 #  ifdef _WIN32
-#   define TRAP __debugbreak()
+#   define TRAP __debugbreak()                                              // halt the program on Windows
+#   define _CRT_SECURE_NO_WARNINGS
 #  else
-#   define TRAP raise( SIGTRAP )// send this signal when something unexpected happen
+#   define TRAP raise(SIGTRAP)                                              // halt the program on Linux
 #  endif
+static constexpr bool is_codingame = false;
 # endif
+# include <cinttypes>                                                       // to bring any integral type definition in the scope
+# include <cstdio>                                                          // to bring all the c standard I/O function (faster than C++ ones)
+static constexpr bool with_state_output = true;
+static constexpr bool with_debug_logger = true;
+static constexpr bool with_info_logger  = true;
+static constexpr bool with_error_logger = true;
+static constexpr bool with_checks       = true;
+static constexpr size_t max_bitstream_bytes = 2048;
 
-# define MEMORY_MEGA_BYTES 500 // how much memory to keep in the local memory handler
-# define BITSTREAM_BYTES 2048  // how much memory to use for bit streams
-# include <cinttypes>          // to bring any integral type definition in the scope
-
-namespace configuration {
-  void initialize() noexcept {}// call this to make sure all static variables are initialized
+# define STRINGIFY_DETAIL(x) #x
+# define STRINGIFY(x) STRINGIFY_DETAIL(x)
+# define Check(condition) if constexpr (with_checks) {\
+  if (__builtin_expect(!(condition),0)) {\
+    fprintf(stderr, "FATAL: condition failure %s at %s:%i(%s)\n", STRINGIFY(condition), __FILE__, __LINE__, __PRETTY_FUNCTION__);\
+    TRAP;\
+  }\
+}
+# define CheckMsg(condition, arguments...) if constexpr (with_checks) {\
+  if (__builtin_expect(!(condition),0)) {\
+    fprintf(stderr, "FATAL: condition failure %s at %s:%i(%s)\n    ", STRINGIFY(condition), __FILE__, __LINE__, __PRETTY_FUNCTION__);\
+    fprintf(stderr, args...);\
+    TRAP;\
+  }\
+}
+namespace logger {
+  template<typename... Args> void debug( const char* format, const Args&... args ) { if constexpr (with_debug_logger) fprintf( stderr, format, args... ); }
+  template<typename... Args> void info( const char* format, const Args&... args ) { if constexpr (with_info_logger) fprintf( stderr, format, args... ); }
+  template<typename... Args> void error( const char* format, const Args&... args ) { if constexpr (with_error_logger) fprintf( stderr, format, args... ); }
 }
