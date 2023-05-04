56c762f31f46b135c83622f626fd723e44f4a95b
Tom Delame <tdelame@gmail.com>
2023-05-04 21:11:05 +0200

    refactor: update my starter project

--- src/bot.cpp
+++ src/bot.cpp
@@ -10,12 +10,19 @@ struct FirstBot: public BotInterface {
     BitStream bs;
     BitStream* bspointer = with_state_output ? &bs : nullptr;
 
-    initialize(controller, output, bspointer);
-    while (true) {
-      step(controller, output, bspointer);
+    try {
+      initialize(controller, output, bspointer);
+      while (true) {
+        step(controller, output, bspointer);
+      }
+    }
+    catch (const std::exception& exc) {
+      fprintf(stderr, "[CRASH] unhandled exception '%s'", exc.what());
+      return 1;
     }
     return 0;
   }
+
   void initialize(BotController& controller, Output& output, BitStream* bs) override {
     (void)controller;
     (void)output;
@@ -33,7 +40,7 @@ struct FirstBot: public BotInterface {
 REGISTER_BOT(FirstBot);
 
 int main() {
-  fprintf(stderr, "[CRASH HANDLER] %s\n", CrashHandler::install() ? "initialized" : "NOT INITIALIZED");
+  fprintf(stderr, "[CRASH HANDLER] %s\n", INSTALL_CRASH_HANDLER ? "initialized" : "NOT INITIALIZED");
   fprintf(stderr, "Bot is a %s version\n", is_codingame ? "CodinGame" : "Local");
   return FirstBot{BotInitializer{}}.loop();
 }
--- src/configuration.h
+++ src/configuration.h
@@ -1,26 +1,45 @@
 # pragma once
-# ifndef __POPCNT__
+// Request immediately optimization on codingame
+# if !defined __POPCNT__ && !defined _WIN32 
 #  pragma GCC optimize("Ofast","unroll-loops","omit-frame-pointer","inline")// improve code optimization
 #  pragma GCC option("arch=native","tune=native","no-zero-upper")           // tuning compilation flags
 #  pragma GCC target("bmi,bmi2,lzcnt,popcnt")                               // bit manipulation
 #  pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2")          // SIMD
 #  pragma GCC target("movbe")                                               // byte swap
 #  pragma GCC target("aes,pclmul,rdrnd")                                    // encryption
-#  define TRAP __builtin_trap()                                             // halt the program in the arena
 #  undef _GLIBCXX_DEBUG
+#  define WITH_LINUX 1
+#  define WITH_WINDOWS 0
 static constexpr bool is_codingame = true;
 # else
-#  include <csignal>
-#  ifdef _WIN32
-#   define TRAP __debugbreak()                                              // halt the program on Windows
-#   define _CRT_SECURE_NO_WARNINGS
-#  else
-#   define TRAP raise(SIGTRAP)                                              // halt the program on Linux
-#  endif
 static constexpr bool is_codingame = false;
 # endif
+
+// Platform-specific configuration
+# if WITH_WINDOWS
+#  define _USE_MATH_DEFINES
+#  define TRAP __debugbreak()
+#  define _CRT_SECURE_NO_WARNINGS
+#  define LIKELY(x) x
+#  define __PRETTY_FUNCTION__ __FUNCSIG__
+#  define START_IGNORE_WARNINGS __pragma(warning(push, 0))
+#  define STOP_IGNORE_WARNINGS __pragma(warning(pop))
+#  pragma warning(disable : 4201 4774)
+# else
+#  include <csignal>
+#  define TRAP (is_codingame ? __builtin_trap() : (void)raise(SIGTRAP))
+#  define LIKELY(x) __builtin_expect(x, 0)
+#  define START_IGNORE_WARNINGS _Pragma("GCC diagnostic ignored \"-Weverything\"")
+#  define STOP_IGNORE_WARNINGS _Pragma("GCC diagnostic pop")
+# endif
+static constexpr bool is_linux = WITH_LINUX;
+static constexpr bool is_windows = WITH_WINDOWS;
+
+// Remaining configuration
 # include <cinttypes>                                                       // to bring any integral type definition in the scope
 # include <cstdio>                                                          // to bring all the c standard I/O function (faster than C++ ones)
+# include <cstring>                                                         // memset
+# include <cmath>
 static constexpr bool with_state_output = true;
 static constexpr bool with_debug_logger = true;
 static constexpr bool with_info_logger  = true;
@@ -31,15 +50,15 @@ static constexpr size_t max_bitstream_bytes = 2048;
 # define STRINGIFY_DETAIL(x) #x
 # define STRINGIFY(x) STRINGIFY_DETAIL(x)
 # define Check(condition) if constexpr (with_checks) {\
-  if (__builtin_expect(!(condition),0)) {\
+  if (LIKELY(!(condition))) {\
     fprintf(stderr, "FATAL: condition failure %s at %s:%i(%s)\n", STRINGIFY(condition), __FILE__, __LINE__, __PRETTY_FUNCTION__);\
     TRAP;\
   }\
 }
-# define CheckMsg(condition, arguments...) if constexpr (with_checks) {\
-  if (__builtin_expect(!(condition),0)) {\
+# define CheckMsg(condition, ...) if constexpr (with_checks) {\
+  if (LIKELY(!(condition))) {\
     fprintf(stderr, "FATAL: condition failure %s at %s:%i(%s)\n    ", STRINGIFY(condition), __FILE__, __LINE__, __PRETTY_FUNCTION__);\
-    fprintf(stderr, arguments);\
+    fprintf(stderr, __VA_ARGS__);\
     TRAP;\
   }\
 }
@@ -48,6 +67,7 @@ namespace logger {
   template<typename... Args> void info( const char* format, const Args&... args ) { if constexpr (with_info_logger) fprintf( stderr, format, args... ); }
   template<typename... Args> void error( const char* format, const Args&... args ) { if constexpr (with_error_logger) fprintf( stderr, format, args... ); }
 }
+// < end of general configuration
 
 // Customize this interface to match the game rules.
 struct Output {
